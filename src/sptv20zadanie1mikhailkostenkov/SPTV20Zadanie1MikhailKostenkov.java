dcc9633e4a89c47502d0858b595bb7df5ff75bc0
Darja Kuznetsova <darja.kuznetsova@ivkhk.ee>
2021-09-28 12:40:59 +0300

    Zadanie 1

---
+++ src/sptv20zadanie1mikhailkostenkov/SPTV20Zadanie1MikhailKostenkov.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20zadanie1mikhailkostenkov;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV20Zadanie1MikhailKostenkov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
